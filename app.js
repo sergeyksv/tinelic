a7d9166697bf207aac60687542852cbac81ff511
Sergey Korotkov <sergey@pushok.com>
2015-01-15 16:18:51 +0300

    Plug newrelic for performance monitoring

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('newrelic')
 var tinyback = require('tinyback');
 
 var cfg = {
---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['[dev] tinelic'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '671b4b29dbe984cfa802ba6a161e1f00bdaa7582',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
