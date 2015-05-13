e8d9be233e4b4413b7f9d882f670f43ad907681b
Sergey Korotkov <sergey@pushok.com>
2015-05-13 21:15:38 +0300

    Action breaakdown fix

--- modules/collectapi.js
+++ modules/collectapi.js
@@ -1,3 +1,5 @@
+/*jslint node: true */
+/*global */
 "use strict";
 var _ = require("lodash");
 var safe = require("safe");
@@ -189,7 +191,7 @@ module.exports.init = function (ctx, cb) {
 			function (cb) {
 				db.collection("page_reqs", safe.sure(cb, function (col) {
 					safe.parallel([
-						function (cb) { ctx.api.mongo.ensureIndex(col,{chash:1}, cb);},
+						function (cb) { ctx.api.mongo.ensureIndex(col,{chash:1,_dt:1}, cb);},
 						function (cb) { ctx.api.mongo.ensureIndex(col,{_idp:1,_dt:1}, cb);}
 					], safe.sure(cb, col));
 				}));
@@ -356,7 +358,7 @@ module.exports.init = function (ctx, cb) {
 
 								var run = {_idp:project._id, _s_pid:body.pid, _s_logger:body.language, _s_host:body.host};
 								res.json({return_value:{"agent_run_id": new Buffer(JSON.stringify(run)).toString('base64')}});
-							}))
+							}));
 						},
 						agent_settings:function () {
 							// seems to be hook to alter agent settings
@@ -370,52 +372,57 @@ module.exports.init = function (ctx, cb) {
 							var body = nrParseBody(req);
 							var run = prefixify(JSON.parse(new Buffer(req.query.run_id, 'base64').toString('utf8')));
 
-							var _dts = new Date( body[1] * 1000.0 )
-								, _dte = new Date( body[2] * 1000.0 )
-								, _dt = new Date( (_dts.getTime() + _dte.getTime()) / 2.0 );
+							var _dts = new Date( body[1] * 1000.0 ),
+								_dte = new Date( body[2] * 1000.0 ),
+								_dt = new Date( (_dts.getTime() + _dte.getTime()) / 2.0 );
 
 							var action_stats = {};
 							_.each(body[body.length-1], function (item) {
 								// grab memory metrics
 								if (item[0].name == "Memory/Physical") {
 									var te = prefixify({
-										_idp: run._idp
-										, _dt: _dt
-										, _dts: _dts
-										, _dte: _dte
-										, _s_type: item[0].name
-										, _s_name: ""
-										, _s_pid: run._s_pid
-										, _s_host: run._s_host
-										, _i_cnt: item[1][0]
-										, _f_val: item[1][1]
-										, _f_own: item[1][2]
-										, _f_min: item[1][3]
-										, _f_max: item[1][4]
-										, _f_sqr: item[1][5]
-									})
+										_idp: run._idp,
+										_dt: _dt,
+										_dts: _dts,
+										_dte: _dte,
+										_s_type: item[0].name,
+										_s_name: "",
+										_s_pid: run._s_pid,
+										_s_host: run._s_host,
+										_i_cnt: item[1][0],
+										_f_val: item[1][1],
+										_f_own: item[1][2],
+										_f_min: item[1][3],
+										_f_max: item[1][4],
+										_f_sqr: item[1][5]
+									});
 									ctx.api.validate.check("metrics",te, safe.sure(nrNonFatal, function () {
-										metrics.insert(te, nrNonFatal)
-									}))
+										metrics.insert(te, nrNonFatal);
+									}));
 								}
 								// grab transaction segments stats
-								var scope = item[0]["scope"];
+								var scope = item[0].scope;
 								if (!scope) return;
 
-								var trnScope = nrParseTransactionName(scope)
-								var trnName = nrParseTransactionName(item[0]["name"])
+								var trnScope = nrParseTransactionName(scope);
+								var trnName = nrParseTransactionName(item[0].name);
+
+								// need to change name of segement if it match
+								// transcation name (scope)
+								if (trnName.name == trnScope.name)
+									trnName+="_seg";
 
 								if( !action_stats[scope] ) {
 									action_stats[scope] = {
-										"_idp": run._idp
-										, "_s_name": trnScope.name
-										, "_s_cat": trnScope.type.split("/", 2)[0]
-										, "_s_type": trnScope.type.split("/", 2)[1]
-										, "_dt": _dt
-										, "_dts": _dts
-										, "_dte": _dte
-										, data: []
-									}
+										"_idp": run._idp,
+										"_s_name": trnScope.name,
+										"_s_cat": trnScope.type.split("/", 2)[0],
+										"_s_type": trnScope.type.split("/", 2)[1],
+										"_dt": _dt,
+										"_dts": _dts,
+										"_dte": _dte,
+										data: []
+									};
 								}
 								action_stats[scope].data.push( {
 									_s_name: trnName.name,
@@ -427,19 +434,19 @@ module.exports.init = function (ctx, cb) {
 									_i_min: Math.round(item[1][3]*1000),
 									_i_max: Math.round(item[1][4]*1000),
 									_i_sqr: Math.round(item[1][5]*1000)
-								})
-							})
+								});
+							});
 							// extra pass to get scope metrics (if any)
 							_.each(body[body.length-1], function (item) {
 								// now process only metrics without scope
-								var scope = item[0]["scope"];
+								var scope = item[0].scope;
 								if (scope) return;
 
 								// but thous that already have details
-								var stat = action_stats[item[0]["name"]]
+								var stat = action_stats[item[0].name];
 								if (!stat) return;
-								
-								var trnName = nrParseTransactionName(item[0]["name"])
+
+								var trnName = nrParseTransactionName(item[0].name);
 
 								stat.data.unshift({
 									_s_name: trnName.name,
@@ -451,15 +458,15 @@ module.exports.init = function (ctx, cb) {
 									_i_min: Math.round(item[1][3]*1000),
 									_i_max: Math.round(item[1][4]*1000),
 									_i_sqr: Math.round(item[1][5]*1000)
-								})
-							})
+								});
+							});
 
 							if (_.size(action_stats)) {
 								_.forEach(_.values(action_stats), function(v) {
 									ctx.api.validate.check("action-stats",v, safe.sure(nrNonFatal, function () {
-										as.insert(v, nrNonFatal)
-									}))
-								})
+										as.insert(v, nrNonFatal);
+									}));
+								});
 							}
 							res.json( { return_value: "ok" } );
 						},
@@ -469,21 +476,21 @@ module.exports.init = function (ctx, cb) {
 
 							_.each(body[body.length - 1], function (item) {
 								item = item[0];
-								var trnName = nrParseTransactionName(item["name"]);
-								var ct = trnName.type.split("/",2)
+								var trnName = nrParseTransactionName(item.name);
+								var ct = trnName.type.split("/",2);
 								var te = {
-									"_idp": run._idp
-									, "_s_name": trnName.name
-									, "_s_cat": ct[0]
-									, "_s_type": ct[1]
-									, "_dt": new Date(item["timestamp"] )
-									, "_i_wt": Math.round(item["webDuration"]*1000)
-									, "_i_tt": Math.round(item["duration"]*1000)
-								}
+									"_idp": run._idp,
+									"_s_name": trnName.name,
+									"_s_cat": ct[0],
+									"_s_type": ct[1],
+									"_dt": new Date(item.timestamp ),
+									"_i_wt": Math.round(item.webDuration*1000),
+									"_i_tt": Math.round(item.duration*1000)
+								};
 								ctx.api.validate.check("actions",te, safe.sure(nrNonFatal, function () {
 									actions.insert(te, nrNonFatal);
-								}))
-							})
+								}));
+							});
 							res.json( { return_value: "ok" } );
 						},
 						error_data:function () {
@@ -518,35 +525,35 @@ module.exports.init = function (ctx, cb) {
 								}
 								ctx.api.validate.check("error",te, safe.sure(function () {
 										console.log(JSON.stringify(te), ne[4]["stack_trace"]);
-										nrNonFatal.apply(this,arguments)
+										nrNonFatal.apply(this,arguments);
 									}, function () {
 									safe.parallel([
 										function(cb) {
 											// save actual error
-											var md5sum = crypto.createHash('md5')
-											md5sum.update(te.exception._s_type)
-											md5sum.update(te._s_message + te.stacktrace.frames.length)
-											te.ehash = md5sum.digest('hex')
+											var md5sum = crypto.createHash('md5');
+											md5sum.update(te.exception._s_type);
+											md5sum.update(te._s_message + te.stacktrace.frames.length);
+											te.ehash = md5sum.digest('hex');
 											action_errors.find({ehash: te.ehash}).sort({_dt: -1}).limit(1).toArray(safe.sure(cb,function(edtl){
 												if (edtl.length)
-													te._dtl = edtl[0]._dtl
+													te._dtl = edtl[0]._dtl;
 												else
 													te._dtl = new Date();
 
-												action_errors.insert(te, cb)
-											}))
+												action_errors.insert(te, cb);
+											}));
 										},
 										function(cb) {
 											// modify error counter for
 											// closest action
 											actions.findAndModify(
 												{_idp:te._idp,_dt: {$gte: te._dt}},
-												{_dt:-1},{$inc: {_i_err: 1}}
-											,cb)
+												{_dt:-1}, {$inc: {_i_err: 1}},
+											cb);
 										}
-									],nrNonFatal)
-								}))
-							})
+									],nrNonFatal);
+								}));
+							});
 
 							res.json( { return_value: "ok" } );
 						},
@@ -563,16 +570,16 @@ module.exports.init = function (ctx, cb) {
 							// request to .net applications long time
 							res.json( { return_value: null } );
 						}
-					}
+					};
 					var fn = nrpc[req.query.method];
 					if (!fn)
-						throw new Error("NewRelic: unknown method " + req.query.method)
+						throw new Error("NewRelic: unknown method " + req.query.method);
 					fn();
 				}, function (err) {
-					nrNonFatal(err)
+					nrNonFatal(err);
 					res.json({exception:{message:err.message}});
-				})
-			})
+				});
+			});
 			ctx.router.get("/ajax/:project", function (req, res, next) {
 				var data = req.query;
 				safe.run(function (cb) {
@@ -589,49 +596,49 @@ module.exports.init = function (ctx, cb) {
 
 					// add few data consistance checks
 					if (data._i_tt > 1000 * 60 * 10)
-						return cb(new Error("Ajax total time is too big > 10 min"))
+						return cb(new Error("Ajax total time is too big > 10 min"));
 
 					if (Math.abs(data._i_tt - data._i_pt - data._i_nt)>1000)
-						return cb(new Error("ajax total time do not match components"))
+						return cb(new Error("ajax total time do not match components"));
 
 					var md5sum = crypto.createHash('md5');
 					md5sum.update(ip);
-					md5sum.update(req.headers['host']);
+					md5sum.update(req.headers.host);
 					md5sum.update(req.headers['user-agent']);
-					md5sum.update(""+parseInt((data._dtp.valueOf()/(1000*60*60))))
+					md5sum.update(""+parseInt((data._dtp.valueOf()/(1000*60*60))));
 					data.shash = md5sum.digest('hex');
 					md5sum = crypto.createHash('md5');
 					md5sum.update(ip);
-					md5sum.update(req.headers['host']);
+					md5sum.update(req.headers.host);
 					md5sum.update(req.headers['user-agent']);
 					md5sum.update(data._dtp.toString());
 					data.chash = md5sum.digest('hex');
-					data._s_name = data.r
-					data._s_url = data.url
-					delete data.url
-					delete data.r
+					data._s_name = data.r;
+					data._s_url = data.url;
+					delete data.url;
+					delete data.r;
 
 					pages.findOne({
 						chash: data.chash,
 						_dt: {$lte: data._dt}
-					}, {sort:{_dt: -1}, hint:{chash:1}}, safe.sure(cb, function (page) {
+					}, {sort:{_dt: -1}}, safe.sure(cb, function (page) {
 						if (page) {
 							data._idpv = page._id;
-							(page._s_route) && (data._s_route = page._s_route);
-							(page._s_uri) && (data._s_uri = page._s_uri);
+							if (page._s_route) data._s_route = page._s_route;
+							if (page._s_uri) data._s_uri = page._s_uri;
 						}
 						ctx.api.validate.check("ajax", data, safe.sure(cb, function(){
 							safe.parallel([
 								function (cb) {
-									ajax.insert(data, cb)
+									ajax.insert(data, cb);
 								},
 								function (cb) {
 									if (!page) return cb();
-									pages.update({_id:page._id}, {$inc:{_i_err: (data._i_code == 200)?0:1}}, cb)
+									pages.update({_id:page._id}, {$inc:{_i_err: (data._i_code == 200)?0:1}}, cb);
 								}
-							],cb)
-						}))
-					}))
+							],cb);
+						}));
+					}));
 				}, function (err) {
 					if (err) {
 						console.log("BAD ajax: " + JSON.stringify(data));
@@ -639,8 +646,8 @@ module.exports.init = function (ctx, cb) {
 					}
 					res.set('Content-Type', 'image/gif');
 					res.send(buf);
-				})
-			})
+				});
+			});
 			ctx.router.get("/browser/:project",function (req, res, next) {
 				var data = req.query;
 				safe.run(function (cb) {
@@ -662,28 +669,28 @@ module.exports.init = function (ctx, cb) {
 
 					// add few data consistance checks
 					if (data._i_tt > 1000 * 60 * 10)
-						return cb(new Error("Page total time is too big > 10 min"))
+						return cb(new Error("Page total time is too big > 10 min"));
 
 					if (Math.abs(data._i_tt - data._i_nt - data._i_lt - data._i_dt)>1000)
-						return cb(new Error("Page total time do not match components"))
+						return cb(new Error("Page total time do not match components"));
 
 					var md5sum = crypto.createHash('md5');
 					md5sum.update(ip);
-					md5sum.update(req.headers['host']);
+					md5sum.update(req.headers.host);
 					md5sum.update(req.headers['user-agent']);
-					md5sum.update(""+parseInt((data._dtp.valueOf()/(1000*60*60))))
+					md5sum.update(""+parseInt((data._dtp.valueOf()/(1000*60*60))));
 					data.shash = md5sum.digest('hex');
 					md5sum = crypto.createHash('md5');
 					md5sum.update(ip);
-					md5sum.update(req.headers['host']);
+					md5sum.update(req.headers.host);
 					md5sum.update(req.headers['user-agent']);
 					md5sum.update(data._dtp.toString());
 					data.chash = md5sum.digest('hex');
 					data._i_err = 0;
-					data._s_uri = data.p
-					data._s_route = data.r
-					delete data.r
-					delete data.p
+					data._s_uri = data.p;
+					data._s_route = data.r;
+					delete data.r;
+					delete data.p;
 
 					ctx.api.validate.check("page", data, safe.sure(cb, function(){
 						pages.insert(data, safe.sure(cb, function (docs) {
@@ -698,25 +705,23 @@ module.exports.init = function (ctx, cb) {
 											var condition = JSON.parse(pageRule._s_condition);
 											condition._id = _id;
 											pages.findOne(condition,{_id:1},safe.sure(cb,function(matched){
-												console.log(condition,!!matched);
 												if (matched) {
 													_.each(pageRule.actions,function(action){
 														if (data[action._s_field] && action._s_type == 'replacer') {
-															console.log(data[action._s_field].replace(RegExp(action._s_matcher),action._s_replacer));
-															data[action._s_field] = data[action._s_field].replace(RegExp(action._s_matcher),action._s_replacer);
+															data[action._s_field] = data[action._s_field].replace(new RegExp(action._s_matcher),action._s_replacer);
 														}
-													})
-													n++
+													});
+													n++;
 												}
 												cb();
-											}))
+											}));
 										},safe.sure(cb,function(){
 											if (n)
 												pages.update({_id:_id},{$set:data},{},cb);
 											else
 												cb();
-										}))
-									}))
+										}));
+									}));
 								},
 								function(cb) {
 									events.update({chash: data.chash, _dt:{$gte:(Date.now()-data._i_tt*2),$lte:data._dt}}, {
@@ -732,7 +737,7 @@ module.exports.init = function (ctx, cb) {
 											pages.update({_id: _id}, {$inc: {_i_err: updates}}, cb);
 										else
 											cb();
-									}))
+									}));
 								},
 								function(cb) {
 									ajax.update({chash: data.chash, _dt:{$gte:(Date.now()-data._i_tt*2),$lte:data._dt}}, {
@@ -746,20 +751,20 @@ module.exports.init = function (ctx, cb) {
 												pages.update({_id: _id}, {$inc: {_i_err: count}}, cb);
 											else
 												cb();
-										}))
-									}))
+										}));
+									}));
 								}
-							], cb)
-						}))
-					}))
+							], cb);
+						}));
+					}));
 				}, function (err) {
 					if (err) {
 						newrelic.noticeError(err);
 					}
 					res.set('Content-Type', 'image/gif');
 					res.send(buf);
-				})
-			})
+				});
+			});
 			// dsn is like http://auth1:auth2@{host}/collect/sentry/{projectid}
 			ctx.router.post( "/sentry/api/store", function( req, res, next ) {
 				safe.run(function(cb) {
@@ -779,7 +784,7 @@ module.exports.init = function (ctx, cb) {
 								_s_value: ge.exception[0].value
 							},
 							stacktrace: { frames: [] }
-						}
+						};
 
 						if (ge.exception[0].stacktrace) {
 							_.each(ge.exception[0].stacktrace.frames, function (frame) {
