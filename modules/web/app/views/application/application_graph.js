25631c70c32fa126adf42c142a218534dfd2241b
Sergey Korotkov <sergey@pushok.com>
2015-05-19 22:14:24 +0300

    Reuse same view for all graphs

--- modules/web/app/views/application/application_graph.js
+++ modules/web/app/views/application/application_graph.js
@@ -10,173 +10,63 @@ define(['tinybone/base', 'lodash',"tinybone/backadapter", "safe", 'dustc!views/a
 			var self = this;
 			var data = this.data;
 
-                          self.$('#apdex-one').highcharts({
-                              chart: {
-                                  type: 'spline',
-                                  zoomType: 'x'
-                              },
-                              title: {
-                                  text: ''
-                              },
-                              xAxis: {
-                                  type: 'datetime'
-                              },
-                              yAxis: [{
-                                  title: {
-                                      text: 'Throughput(apdex)'
-                                  },
-                                  min: 0,
-                                  max: data.actapdexMax
-                              }
-                              ],
-                              plotOptions: {
-                                  series: {
-                                      marker: {
-                                          enabled: false
-                                      },
-                                      animation: false
-                                  }
-                              },
-                              legend: {
-                                  enabled: false
-                              },
-                              credits: {
-										enabled: false
-							  },
-                              series: [
-                                  {
-                                      name: 'apdex',
-                                      yAxis: 0,
-                                      data: data.actapdex,
-                                      color: "brown",
-                                      type: 'area',
-                                      fillColor: {
-                                          linearGradient: {
-                                              x1: 0,
-                                              y1: 0,
-                                              x2: 0,
-                                              y2: 1
-                                          },
-                                          stops: [
-                                              [0, '#F2CAC8'],
-                                              [1, 'white']
-                                          ]
-                                      }
-                                  }
-                              ]
-                          })
-                          self.$('#rpm-one').highcharts({
-                              chart: {
-                                  type: 'spline',
-                                  zoomType: 'x'
-                              },
-                              title: {
-                                  text: ''
-                              },
-                              xAxis: {
-                                  type: 'datetime'
-                              },
-                              yAxis: [{
-                                  title: {
-                                      text: 'Throughput (rpm)'
-                                  },
-                                  min: 0,
-                                  max: data.actrpmmax
-                              }
-                              ],
-                              plotOptions: {
-                                  series: {
-                                      marker: {
-                                          enabled: false
-                                      },
-                                      animation: false
-                                  }
-                              },
-                              legend: {
-                                  enabled: false
-                              },
-                              credits: {
-										enabled: false
-							  },
-                              series: [
-                                  {
-                                      name: 'rpm',
-                                      yAxis: 0,
-                                      data: data.actrpm,
-                                      color: "green",
-                                      type: 'area',
-                                      fillColor: {
-                                          linearGradient: {
-                                              x1: 0,
-                                              y1: 0,
-                                              x2: 0,
-                                              y2: 1
-                                          },
-                                          stops: [
-                                              [0, 'lightgreen'],
-                                              [1, 'white']
-                                          ]
-                                      }
-                                  }
-                              ]
-                          })
-                          self.$('#time-one').highcharts({
-                              chart: {
-                                  type: 'spline',
-                                  zoomType: 'x'
-                              },
-                              title: {
-                                  text: ''
-                              },
-                              xAxis: {
-                                  type: 'datetime'
-                              },
-                              yAxis: [{
-                                  title: {
-                                      text: 'Timing (s)'
-                                  },
-                                  min: 0,
-                                  max: data.ttServerMax
-                              }
-                              ],
-                              plotOptions: {
-                                  series: {
-                                      marker: {
-                                          enabled: false
-                                      },
-                                      animation: false
-                                  }
-                              },
-                              legend: {
-                                  enabled: false
-                              },
-                              credits: {
-										enabled: false
-							  },
-                              series: [
-                                  {
-                                      name: 'rpm',
-                                      yAxis: 0,
-                                      data: data.ttServer,
-                                      color: "blue",
-                                      type: 'area',
-                                      fillColor: {
-                                          linearGradient: {
-                                              x1: 0,
-                                              y1: 0,
-                                              x2: 0,
-                                              y2: 1
-                                          },
-                                          stops: [
-                                              [0, 'lightblue'],
-                                              [1, 'white']
-                                          ]
-                                      }
-                                  }
-                              ]
-                          })
+			self.$('#graph').highcharts({
+			  chart: {
+			      type: 'spline',
+			      zoomType: 'x'
+			  },
+			  title: {
+			      text: ''
+			  },
+			  xAxis: {
+			      type: 'datetime'
+			  },
+			  yAxis: [{
+			      title: {
+			          text: this.get('name')
+			      },
+			      min: 0,
+			      max: this.get('max')
+			  }
+			  ],
+			  plotOptions: {
+			      series: {
+			          marker: {
+			              enabled: false
+			          },
+			          animation: false
+			      }
+			  },
+			  legend: {
+			      enabled: false
+			  },
+			  credits: {
+						enabled: false
+			  },
+			  series: [
+			      {
+			          name: this.locals.name,
+			          yAxis: 0,
+			          data: data,
+			          color: "brown",
+			          type: 'area',
+			          fillColor: {
+			              linearGradient: {
+			                  x1: 0,
+			                  y1: 0,
+			                  x2: 0,
+			                  y2: 1
+			              },
+			              stops: [
+			                  [0, this.get('color')],
+			                  [1, 'white']
+			              ]
+			          }
+			      }
+			  ]
+			});
 		}
-	})
+	});
 	View.id = "views/application/application_graph";
 	return View;
-})
+});
