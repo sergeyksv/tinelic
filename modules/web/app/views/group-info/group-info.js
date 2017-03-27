737b87313ec496e0fefcddef27b4e0cec70bb9f3
alexey.sharov@pushok.com <alexey.sharov@pushok.com>
2017-03-27 15:26:37 +0300

    optimized code of sorting

--- modules/web/app/views/group-info/group-info.js
+++ modules/web/app/views/group-info/group-info.js
@@ -20,80 +20,35 @@ define(['tinybone/base', 'lodash','tinybone/backadapter', 'safe','dustc!views/gr
 		preRender: function () {
 				var locals = this.locals;
 				var project0 = this.data.teams.projects;
-				if (!this.data.sortType) {
-						;//console.log("this.data.sortType", this.data.sortType)
-				} else {
-					if (this.data.sortType == "asc") {
-							if (this.data.sortByF == "name")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.name');
-							if (this.data.sortByF == "dtlActions")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.errAck.dtlActions');
-							if (this.data.sortByF == "dtlPages")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.errAck.dtlPages');
-							if (this.data.sortByF == "ApdexServer")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.apdex.server');
-							if (this.data.sortByF == "ApdexClient")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.apdex.client');
-							if (this.data.sortByF == "ApdexAjax")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.apdex.ajax');
-							if (this.data.sortByF == "ThroughputServer")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.server.r');
-							if (this.data.sortByF == "ThroughputClient")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.client.r');
-							if (this.data.sortByF == "ThroughputAjax")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.ajax.r');
-							if (this.data.sortByF == "TimeServer")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.server.etu');
-							if (this.data.sortByF == "TimeClient")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.client.etu');
-							if (this.data.sortByF == "TimeAjax")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.ajax.etu');
-							if (this.data.sortByF == "erateServer")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.server.e');
-							if (this.data.sortByF == "erateClient")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.client.e');
-							if (this.data.sortByF == "erateAjax")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.ajax.e');
-					}
-					if (this.data.sortType == "desc") {
-							if (this.data.sortByF == "name")
-								this.data.teams.projects = safe.sortBy(project0, '_t_proj.name').reverse();
-								if (this.data.sortByF == "dtlActions")
-									this.data.teams.projects = safe.sortBy(project0, '_t_proj.errAck.dtlActions').reverse();
-								if (this.data.sortByF == "dtlPages")
-									this.data.teams.projects = safe.sortBy(project0, '_t_proj.errAck.dtlPages').reverse();
-								if (this.data.sortByF == "ApdexServer")
-									this.data.teams.projects = safe.sortBy(project0, '_t_proj.apdex.server').reverse();
-								if (this.data.sortByF == "ApdexClient")
-									this.data.teams.projects = safe.sortBy(project0, '_t_proj.apdex.client').reverse();
-								if (this.data.sortByF == "ApdexAjax")
-									this.data.teams.projects = safe.sortBy(project0, '_t_proj.apdex.ajax').reverse();
-								if (this.data.sortByF == "ThroughputServer")
-									this.data.teams.projects = safe.sortBy(project0, '_t_proj.server.r').reverse();
-								if (this.data.sortByF == "ThroughputClient")
-									this.data.teams.projects = safe.sortBy(project0, '_t_proj.client.r').reverse();
-								if (this.data.sortByF == "ThroughputAjax")
-									this.data.teams.projects = safe.sortBy(project0, '_t_proj.ajax.r').reverse();
-								if (this.data.sortByF == "TimeServer")
-									this.data.teams.projects = safe.sortBy(project0, '_t_proj.server.etu').reverse();
-								if (this.data.sortByF == "TimeClient")
-									this.data.teams.projects = safe.sortBy(project0, '_t_proj.client.etu').reverse();
-								if (this.data.sortByF == "TimeAjax")
-									this.data.teams.projects = safe.sortBy(project0, '_t_proj.ajax.etu').reverse();
-								if (this.data.sortByF == "erateServer")
-									this.data.teams.projects = safe.sortBy(project0, '_t_proj.server.e').reverse();
-								if (this.data.sortByF == "erateClient")
-									this.data.teams.projects = safe.sortBy(project0, '_t_proj.client.e').reverse();
-								if (this.data.sortByF == "erateAjax")
-									this.data.teams.projects = safe.sortBy(project0, '_t_proj.ajax.e').reverse();
-					}
+				var sortMap = {
+												"name": "_t_proj.name",
+												"dtlActions": "_t_proj.errAck.dtlActions",
+												"dtlPages": "_t_proj.errAck.dtlPages",
+												"ApdexServer": "_t_proj.apdex.server",
+												"ApdexClient": "_t_proj.apdex.client",
+												"ApdexAjax": "_t_proj.apdex.ajax",
+												"ThroughputServer": "_t_proj.server.r",
+												"ThroughputClient": "_t_proj.client.r",
+												"ThroughputAjax": "_t_proj.ajax.r",
+												"TimeServer": "_t_proj.server.etu",
+												"TimeClient": "_t_proj.client.etu",
+												"TimeAjax": "_t_proj.ajax.etu",
+												"erateServer": "_t_proj.server.e",
+												"erateClient": "_t_proj.client.e",
+												"erateAjax": "_t_proj.ajax.e"
+											};
+				if	(this.data.sortType) {
+					project0 = safe.sortBy(project0, sortMap[this.data.sortByF]);
+
+					if (this.data.sortType == "desc")
+						project0 = project0.reverse();
+
+					this.data.teams.projects = project0;
 				};
 				if (!locals.pageCount) {
 						// set default data
 						locals.pageCount = Math.ceil(this.data.teams.projects.length/10);
 						locals.currentPage = this.data.tmpCurPage;
-						locals.sortField = this.data.sortByF;
-						locals.sortType = this.data.sortType;
 				}
 				// update paging helper variables
 				locals.leftlistEnd = locals.currentPage*10-1;
@@ -104,8 +59,8 @@ define(['tinybone/base', 'lodash','tinybone/backadapter', 'safe','dustc!views/gr
 				};
 		},
 		postRender:function () {
+// zebra
 			var trbreak = this.$('#trbreak');
-//			trbreak.tablesorter();
 			for (var i = 1; i < trbreak[0].rows.length; i+=2) {
 				trbreak[0].rows[i].className = "odd";
 			}
