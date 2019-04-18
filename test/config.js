2e79e6b27ea21d046fda5ac60dbf54ed66a62a40
Kirill Chistiakov <kirill.chistiakov@pushok.com>
2019-04-18 13:42:29 +0300

    test config

--- test/config.js
+++ test/config.js
@@ -1,13 +1,14 @@
 module.exports = {
-	env:"production",
-	automated:true,
-	mongo:{
-		main:{
-			db: "tinelic_test",
+	env: 'production',
+	automated: true,
+	mongo: {
+		main: {
+			db: 'tinelic_test',
+			host: process.env.CI ? 'mongo' : 'localhost'
 		}
 	},
-	server:{
-		port:8080,
-		ssl_port:false
+	server: {
+		port: 8080,
+		ssl_port: false
 	}
 };
