ffe71074e68a7eda7fcfaaa532d9346ed43a384f
Kirill Chistiakov <kirill.chistiakov@pushok.com>
2019-04-30 18:25:12 +0300

    add bt to the pages

---
+++ modules/web/app/views/pages/pages_left_list.js
@@ -0,0 +1,36 @@
+/* global define */
+define([
+	'tinybone/base',
+	'lodash',
+	'dustc!views/pages/pages_left_list.dust',
+	'dustc!views/common/left_list.dust',
+	'bootstrap-table',
+	'bootstrap-table-cookie'
+], (tb, _) => {
+	let view = tb.View;
+	let View = view.extend({
+		id: 'views/pages/pages_left_list',
+		preRender: function () {
+			this.locals.leftList = _.reduce(this.data.data, (r, d) => {
+				let o = {_id: d._id};
+				_.forEach(d.value, (v, k) => {o[k] = v;});
+				r.push(o);
+				return r;
+			}, []);
+		},
+		postRender: function () {
+			view.prototype.postRender.call(this);
+			this.$el.find('#leftList').bootstrapTable({
+				classes: 'table-sm',
+				pagination: true,
+				pageList: [],
+				cookie: true,
+				cookieIdTable: 'leftListPages',
+				cookieStorage: 'sessionStorage',
+				search: true
+			});
+		}
+	});
+	View.id = 'views/pages/pages_left_list';
+	return View;
+});
