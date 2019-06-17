3ec929063724129af31e38c3a6c414e2baaecd80
Kirill Chistiakov <kirill.chistiakov@pushok.com>
2019-06-17 16:49:00 +0300

    fix total numbers on main page

--- modules/web/app/routes/index.js
+++ modules/web/app/routes/index.js
@@ -1,13 +1,13 @@
 'use strict';
 /* global define */
 define(['require', 'tinybone/backadapter', 'safe', 'lodash', 'feed/mainres', 'moment'], (require, api, safe, _, feed, moment) => (req, res, cb) => {
-	var quant = 5;
-	safe.parallel({
+	let quant = 5;
+	safe.auto({
 		view: (cb) => require(['views/index/index'], (view) => safe.back(cb, null, view), cb),
 		data: (cb) => {
-			var tolerance = 5 * 60 * 1000;
-			var dtend = parseInt((Date.now() + tolerance) / tolerance) * tolerance;
-			var dtstart = res.locals.dtend - 20 * 60 * 1000;
+			let tolerance = 5 * 60 * 1000;
+			let dtend = parseInt((Date.now() + tolerance) / tolerance) * tolerance;
+			let dtstart = res.locals.dtend - 20 * 60 * 1000;
 			api('web.getFeed', res.locals.token, {
 				_t_age: quant + 'm', feed: 'mainres.homeInfo', params: {
 					quant: quant, fv: req.query.fv, filter: {
@@ -16,9 +16,9 @@ define(['require', 'tinybone/backadapter', 'safe', 'lodash', 'feed/mainres', 'mo
 				}
 			}, safe.sure(cb, (r) => {
 				r.forEach((r) => {
-					var period;
-					var errAck = r.result.errAck;
-					var Apdex = {}; var Server = {}; var Client = {}; var Ajax = {};
+					let period;
+					let errAck = r.result.errAck;
+					let Apdex = {}; let Server = {}; let Client = {}; let Ajax = {};
 					Client.r = Client.e = Client.etu = 0;
 					Apdex.client = Apdex.server = Apdex.ajax = 0;
 					Ajax.r = Ajax.e = Ajax.etu = 0;
@@ -81,39 +81,44 @@ define(['require', 'tinybone/backadapter', 'safe', 'lodash', 'feed/mainres', 'mo
 		teams: (cb) => api('users.getCurrentUser', res.locals.token, {}, safe.sure(cb, (usr) => {
 			if (!usr.favorites || !usr.favorites.length) {
 				api('assets.getTeams', res.locals.token, { _t_age: quant + 'm' }, safe.sure(cb, (_teams) => {
-					var _fv = 'ALL';
+					let _fv = 'ALL';
 					cb(null, _teams, _fv);
 				}));
 			} else {
 				if (req.query.fv == 'ALL') {
 					api('assets.getTeams', res.locals.token, { _t_age: quant + 'm' }, safe.sure(cb, (_teams) => {
-						var _fv = 'ALL';
+						let _fv = 'ALL';
 						cb(null, _teams, _fv);
 					}));
 				}
 				else {
-					var idf = _.map(usr.favorites, '_idf');
+					let idf = _.map(usr.favorites, '_idf');
 					api('assets.getTeams', res.locals.token, { _t_age: quant + 'm', filter: { _id: { $in: idf } } }, safe.sure(cb, (_teams) => {
-						var _fv = 'FAV';
+						let _fv = 'FAV';
 						cb(null, _teams, _fv);
 					}));
 
 				}
 			}
-		}))
-	}, safe.sure(cb, (r) => {
-		var _fv = r.teams[1];
-		r.teams = r.teams[0];
-		_.forEach(r.teams, (team) => {
-			var projects = {};
-			_.forEach(r.data, (proj) => projects[proj._id] = proj);
-			_.forEach(team.projects, (proj) => proj._t_proj = projects[proj._idp]);
+		})),
+		metrics: ['data', 'teams', (cb, r) => {
+			let _fv = r.teams[1];
+			r.teams = r.teams[0];
 
-			var tmetrics = {};
-			_.forEach(team.projects, (proj) => _.assign(tmetrics, _.pick(proj._t_proj, 'apdex', 'server', 'client', 'errAck', 'ajax'),
-				(oval, sval, key) => {
-					var memo = {};
-					var rpm = 1, k;
+			for (let team of r.teams) {
+
+				let projects = {};
+				for (const proj of r.data) {
+					projects[proj._id] = proj;
+				}
+				for (const proj of team.projects) {
+					proj._t_proj = projects[proj._idp];
+				}
+
+				let tmetrics = {};
+				_.forEach(team.projects, (proj) => _.assignInWith(tmetrics, _.pick(proj._t_proj, 'apdex', 'server', 'client', 'errAck', 'ajax'), (oval, sval, key) => {
+					let memo = {};
+					let rpm = 1, k;
 					if (key == 'apdex') for (k in sval) {
 						if (_.isUndefined(proj._t_proj[k].r)) rpm = 1; else rpm = proj._t_proj[k].r;
 						if (_.isUndefined(oval)) memo[k] = sval[k] * rpm; else memo[k] = oval[k] + sval[k] * rpm;
@@ -122,25 +127,32 @@ define(['require', 'tinybone/backadapter', 'safe', 'lodash', 'feed/mainres', 'mo
 							if ((k == 'e') || (k == 'etu')) rpm = sval.r; else rpm = 1;
 							if (_.isUndefined(oval)) memo[k] = sval[k] * rpm; else memo[k] = oval[k] + sval[k] * rpm;
 						}
-					memo;
+					return memo;
 				}));
 
-			_.forEach(tmetrics.apdex, (stat, key) => tmetrics.apdex[key] = stat / tmetrics[key].r);
-			_.forEach(_.pick(tmetrics, 'server', 'client', 'ajax'), (stat, key) => {
-				tmetrics[key].e = stat.e / tmetrics[key].r;
-				tmetrics[key].etu = stat.etu / tmetrics[key].r;
-			});
-			if (tmetrics.server) {
-				tmetrics.server.mem = tmetrics.server.mem / tmetrics.server.proc;
+				_.forEach(tmetrics.apdex, (stat, key) => tmetrics.apdex[key] = stat / tmetrics[key].r);
+
+				_.forEach(_.pick(tmetrics, 'server', 'client', 'ajax'), (stat, key) => {
+					tmetrics[key].e = stat.e / tmetrics[key].r;
+					tmetrics[key].etu = stat.etu / tmetrics[key].r;
+				});
+
+				if (tmetrics.server) {
+					tmetrics.server.mem = tmetrics.server.mem / tmetrics.server.proc;
+				}
+
+				team.t_metrics = tmetrics;
 			}
-			team.t_metrics = tmetrics;
-		});
+
+			cb(null, _fv);
+		}]
+	}, safe.sure(cb, (r) => {
 		res.renderX({
 			view: r.view,
 			data: {
 				title: 'Tinelic - Home',
 				teams: r.teams,
-				_fv: _fv
+				_fv: r.metrics
 			}
 		});
 	}));
